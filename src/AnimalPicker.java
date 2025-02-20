5cad8f74d4d74b0a0a8f997cc866da0792cb285d
hcort26 <144395316+hcort26@users.noreply.github.com>
2025-02-19 19:40:25 -0500

    added dog and cat ascii art

--- src/AnimalPicker.java
+++ src/AnimalPicker.java
@@ -10,10 +10,10 @@ public class AnimalPicker {
             userChoice = scanner.nextLine().trim().toLowerCase();
 
             if (userChoice.equals("dog")) {
-                System.out.println();
+                System.out.println("     |\\_/|                  \n     | @ @   Woof! \n     |   <>              _  \n     |  _/\\------____ ((| |))\n     |               `--\' |   \n ____|_       ___|   |___.\' \n/_/_____/____/_______|");
                 break;
             } else if (userChoice.equals("cat")) {
-                System.out.println();
+                System.out.println("      |\\      _,,,---,,_\nZZZzz /,`.-\'`\'    -.  ;-;;,_\n     |,4-  ) )-,_. ,\\ (  `\'-\'\n    \'---\'\'(_/--\'  `-\'\\_)  ");
                 break;
             } else {
                 System.out.println("Invalid option. Please enter 'dog' or 'cat'.");
